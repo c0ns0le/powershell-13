bd7d0c957c07ccee879e633548cc6d40ba1525c2
aimless <m.hackl@gmail.com>
2015-01-13 16:57:17 +0100

    brute-force network spidering

---
+++ corel.ps1
@@ -0,0 +1,10 @@
+﻿#brute force attempt to verify software installation
+100..600 | % {
+        $computer =  "AT1P" + $_.ToString().PadLeft(3, '0')
+        if ((Test-Connection -ComputerName $computer -Quiet)) {
+            $path = "\\" + $computer + "\C$\Program Files (x86)\Corel\CorelDRAW Graphics Suite 13\Programs\CorelDRW.exe"
+            if (test-path $path) {
+                write-host $computer
+            }
+        }
+}
