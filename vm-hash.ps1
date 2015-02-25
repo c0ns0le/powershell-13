72c24c07d6106faea764417fb6cd4f2168724779
aimless <aimless@users.noreply.github.com>
2015-02-25 14:55:14 +0100

    conf path needs to be available before loading the assembly; new tool

---
+++ vm-hash.ps1
@@ -0,0 +1,5 @@
+﻿function generatePwd($string){
+    $p = Start-Process -FilePath .\runCrypto.bat -ArgumentList $string -Wait -passthru -NoNewWindow;
+    $hash = $(cat CryptPwd).Split('\n') | where { $_.Substring(0,3) -eq "pwd" } | % { $_.Split('=')[1] }
+    return $hash
+}
